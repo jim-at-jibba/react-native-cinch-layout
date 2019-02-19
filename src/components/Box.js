02105ee53241f8791f921a342068fcbfdbbbc5cc
James <Best>
2019-02-19 21:15:18 +0000

    Added proptypes and linting

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,15 +1,26 @@
-import * as React from "react";
-import { View, Text } from "react-native";
-import styled from 'styled-components/native'
+import * as React from 'react';
+import styled from 'styled-components/native';
 import PropTypes from 'prop-types';
-import { flex, drawDebug } from "../helpers";
+import { flex, drawDebug } from '../helpers';
 
 export const CinchBox = styled.View`
   ${props => props.debug && drawDebug()};
   ${props => flex(props)};
 `;
 
+CinchBox.propTypes = {
+  /**
+   * Enable if you want the geometry of the grid exposed
+   */
+  debug: PropTypes.bool,
+
+  /**
+   * Interface for setting the `flex` css property
+   */
+  flex: PropTypes.number,
+};
+
 CinchBox.defaultProps = {
   flex: 1,
-  debug: false
+  debug: false,
 };
