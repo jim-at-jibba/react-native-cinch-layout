bd61d945623aa5a517c9e6ecaa32d107498be922
James Best <jim@justjibba.net>
2019-07-02 07:52:59 +0100

    Added types

---
+++ index.d.ts
@@ -0,0 +1,49 @@
+import * as React from 'react'
+
+export interface CinchBoundsProps {
+  /**
+   * Enable if you want the geometry of the grid exposed
+   */
+  debug: boolean
+
+  /**
+   * Interface for setting the `flex` css property
+   */
+  flex?: number
+
+  /**
+   * The primary axis the children should be in line with
+   */
+  flexDirection?: 'horizontal' | 'vertical'
+
+  /**
+   * Whether children should wrap when there's no more room
+   * on the primary axis.
+   */
+  wrap?: boolean
+
+  /**
+   * Alignment of children along the vertical axis
+   */
+  valign?: 'top' | 'center' | 'bottom'
+
+  /**
+   * Alignment of children along the horizontal axis
+   */
+  halign?: 'left' | 'center' | 'right'
+}
+
+export interface CinchBoxProps {
+  /**
+   * Enable if you want the geometry of the grid exposed
+   */
+  debug?: boolean
+
+  /**
+   * Interface for setting the `flex` css property
+   */
+  flex?: number
+}
+
+export class CinchBounds extends React.Component<CinchBoundsProps> {}
+export class Box extends React.Component<CinchBoxProps> {
